880a27fbe28677dc0749b2da2a19c313845ea977
caleb <calebgamercraft@gmail.com>
2022-03-21 17:28:33 -0500

    El ejercicio

---
+++ 1.ExpresionesAlgebraicas/ejer4.py
@@ -0,0 +1,9 @@
+"""
+4. Dada una ecuación algebraica de 4 incógnitas, codifica la expresión en 
+partes simples y desarrolla la ecuación en un archivo con nombre ecuacion.py
+
+a. Calcula su resultado evaluado con valores constantes para las incógnitas.
+
+b. Cambia los valores constantes de las incógnitas, compile y revise el resultado.
+
+"""
