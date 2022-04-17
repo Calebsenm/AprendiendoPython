63c26caca250f978322fa1c3d9b564c150470490
caleb <calebgamercraft@gmail.com>
2022-04-17 18:42:28 -0500

    Quedó terminado el oso llogui :D

---
+++ Modulo1/6_Programitas/MiniPersonaje.py
@@ -0,0 +1,58 @@
+"""
+ Acciones de aprendizaje
+1. Crea un texto personalizado usando la aplicación online patorjk. 
+
+2. Implementa el texto personalizado y agrégale un personaje,
+seleccionando alguno de la galería.
+
+Recomendación: inicia con diseños de pocas líneas.
+Herramienta ASCII Art Archive (2020). 
+
+3. Escribe un programa donde se imprima un título personalizado
+y un diseño de un personaje, en el que se evidencie el uso de ASCII art.
+
+17 / 04 / 2022   6:41 PM :D
+
+"""
+ 
+print("  ___ ___ ___  ") 
+print(" / _ (_-</ _ \ ")
+print(" \___/__/\___/ ")
+print("")
+
+print("   _,-\"\"`\"\"-~`)                                         ")
+print(" (`~_,=========\                                        ")
+print("  |---,___.-.__,\                                       ")
+print("  |        o     \ ___  _,,,,_     _.--.                ")
+print("   \      `^`    /`_.-\"~      `~-;`     \              ")
+print("    \_      _  .'                 `,     |             ")
+print("       |`-                           \'__/               ")
+print("      /                      ,_       \  `'-.            ")
+print("     /    .-\"\"~~--.            `\"-,   ;_    /          ")
+print("    |              \               \  | `""`             ")
+print("     \__.--'`\"-.   /_               |'                  ")
+print("                `\"`  `~~~---..,     |                   ")
+print("                              \ _.-'`-.                  ")
+print("                               \       \                 ")
+print("                                '.     /                 ")
+print("                                  `\"~\"`                ")
+  
+ 
+ 
+
+
+
+ 
+ 
+  
+   
+      
+    
+   
+  
+
+
+
+
+
+                              
