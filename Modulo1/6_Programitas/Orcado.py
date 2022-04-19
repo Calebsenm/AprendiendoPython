e83f53922a79ad966fa09c637d80d015c331d7c2
caleb <calebgamercraft@gmail.com>
2022-04-19 13:11:51 -0500

    Version 1

--- Modulo1/6_Programitas/Orcado.py
+++ Modulo1/6_Programitas/Orcado.py
@@ -34,12 +34,151 @@ de partida perdida y se lleva al usuario a seleccionar otra categoría
 para seguir jugando.
 
 
-18/04/2022    
+18/04/2022             / 4: 42 pm 
+ 
+terminado el 
+19 /04/2022     a las 1:08    aproximadamente 10 horas de investigacion y desarrolloo
 
+
+me gustó mucho el resultado es mi primer juego siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 """
+from operator import index
 from os import system
 import random
 
+def funcionDelmuñeco():
+    ####  aqui se imprimirá el muñequito
+    if cantidad_intentos == 1:
+        print("""
+
+
+
+
+
+                              /___\\
+                
+        """)
+    elif cantidad_intentos ==2:
+        print("""
+
+
+
+
+
+                               / \\
+                              /___\\
+                """)
+    elif cantidad_intentos ==3:
+        print("""
+
+
+
+
+                                |
+                                |
+                               / \\
+                              /___\\
+                """)
+    elif cantidad_intentos ==4:
+        print("""
+
+
+                                |
+                                |
+                                |
+                                |
+                               / \\
+                              /___\\
+                """)
+    elif cantidad_intentos ==4:
+        print("""
+
+
+                                |
+                                |
+                                |
+                                |
+                               / \\
+                              /___\\
+        """)
+    elif cantidad_intentos ==5:
+        print("""
+                    
+                                |
+                                |                            
+                                |
+                                |
+                                |
+                                |
+                               / \\
+                              /___\\
+        """)
+    elif cantidad_intentos ==6:
+        print("""
+                    _____________
+                                |
+                                |                            
+                                |
+                                |
+                                |
+                                |
+                               / \\
+                              /___\\
+        """)
+
+    elif cantidad_intentos ==7:
+        print("""
+                       _________
+                      |         |
+                      |         |                            
+                                |
+                                |
+                                |
+                                |
+                               / \\
+                              /___\\
+        """)
+
+    elif cantidad_intentos ==8:
+        print("""
+                       _________
+                      |         |
+                      |         |                            
+                    \\0/        |
+                                |
+                                |
+                                |
+                               / \\
+                              /___\\
+        """)
+    elif cantidad_intentos ==9:
+        print("""
+                       _________
+                      |         |
+                      |         |                            
+                     \O/        | 
+                      |         |
+                                |
+                                |
+                                |
+                               / \\
+                              /___\\
+        """)
+    elif cantidad_intentos ==10:
+        print("""
+                       _________
+                      |         |
+                      |         |                            
+                     \O/        | 
+                      |         |
+                     / \        |
+                                |
+                                |
+                               / \\
+                              /___\\
+        """)
+
+            ####
 
 monigote = """
   *----*      
@@ -50,13 +189,10 @@ monigote = """
        |
       / \\
      /___\\
-
 """
-
-
 system("cls")
 
-M = [["colombia","brazil","ecuador","Peru","venezuela"],
+M = [["colombia","brazil","ecuador","peru","venezuela"],
      ["tamarindo","aguacate","mandarina","banano","nispero"],
      ["vallenato","opera","Rock","bachata","salsa"],
      ["loro","ornitorrinco","hormiga","caballo","saltamontes"],
@@ -85,57 +221,368 @@ while True:
         break
 
     if Opcion == "1":
+
+        ###########################################################
+        cantidad_intentos = 0
         system("cls")
-        print("Has elegido la categoria Países")
+        
 
         #logica del juego! :D
         Palabra_Aleatoria = random.choice(M[0])
         Lista = len( Palabra_Aleatoria)
-        indice =M[0].index(Palabra_Aleatoria)
-        
-        Lista_de_la_palabra = []
+        indice = M[0].index(Palabra_Aleatoria)
 
+        Lista_de_la_palabra = []
         #imprime las barras de abajo
         for i in range(Lista):
-            Lista_de_la_palabra.append(["_"])
-        
+            Lista_de_la_palabra.append("_")
 
         while True:
+            system("cls")
+            #imprime el monigote
+            print("Has elegido la categoria Países")
             
+            funcionDelmuñeco()
+            if cantidad_intentos == 10:
+                print("el juego ha terminado")
+                break
+
+            
+            #imprime el arrglo
             for i in range(Lista):
-                for j in range(1):
-                    print(Lista_de_la_palabra[i][j],end = ' ')
+                print(Lista_de_la_palabra[i],end = ' ')
+            print("")
             
-            print()
-            Palabra = input("Ingresa una letra > ")
+
+            Letra = input("Ingresa una letra > ")
 
             #Este convierte la palabra optenida a una lista
-            NuevoArreglo = Palabra.split()
             
-
-            if Palabra in M[0][indice]:
+            NuevoArreglo_Palabra = list(Palabra_Aleatoria)
+            
+            #condiciona si la letra exite dentro del arreglo
+            if Letra in M[0][indice]:
                 print("Palabra corecta")
 
+                #esta buscará la palabra y la colocará en la posicion
+                def Indices(lst,item):
+                    return [i for i,x in enumerate(lst) if x ==item]
+                Indices_Palabras_mencionadas = Indices(NuevoArreglo_Palabra,Letra)
+
+                Cuenta_Areglo = len(Indices_Palabras_mencionadas)
+
+                #coloca las palabras en el arreglo
+                for i in range(Cuenta_Areglo):
+
+                    b = Indices_Palabras_mencionadas[i]
+                    
+                    Lista_de_la_palabra[b] = NuevoArreglo_Palabra[b]
+
+                    print()
+                #Verificará si ganó el juego
+                if "_" in Lista_de_la_palabra:
+                    continue
+                else:
+                    system("cls")
+                    print("¡ Felicidades ha gado el juego ! ")
+                    break
 
             else:
                 print("la palabra no existe ")
+                cantidad_intentos = cantidad_intentos + 1
+        ##################################################################
 
 
     elif Opcion == "2":
+        
+        ####################################
+        ###########################################################
+        cantidad_intentos = 0
         system("cls")
-        print("has elegido la categoria Frutas")
+        
+
+        #logica del juego! :D
+        Palabra_Aleatoria = random.choice(M[1])
+        Lista = len( Palabra_Aleatoria)
+        indice = M[1].index(Palabra_Aleatoria)
+
+        Lista_de_la_palabra = []
+        #imprime las barras de abajo
+        for i in range(Lista):
+            Lista_de_la_palabra.append("_")
+
+        while True:
+            system("cls")
+            print("has elegido la categoria Frutas")
+            #imprime el monigote
+            funcionDelmuñeco()
+            if cantidad_intentos == 10:
+                print("el juego ha terminado")
+                break
+
+
+            #imprime el arrglo
+            for i in range(Lista):
+                print(Lista_de_la_palabra[i],end = ' ')
+            print("")
+            
+
+            Letra = input("Ingresa una letra > ")
+
+            #Este convierte la palabra optenida a una lista
+            
+            NuevoArreglo_Palabra = list(Palabra_Aleatoria)
+            
+            #condiciona si la letra exite dentro del arreglo
+            if Letra in M[1][indice]:
+                print("Palabra corecta")
+
+                #esta buscará la palabra y la colocará en la posicion
+                def Indices(lst,item):
+                    return [i for i,x in enumerate(lst) if x ==item]
+                Indices_Palabras_mencionadas = Indices(NuevoArreglo_Palabra,Letra)
+
+                Cuenta_Areglo = len(Indices_Palabras_mencionadas)
+
+                #coloca las palabras en el arreglo
+                for i in range(Cuenta_Areglo):
+
+                    b = Indices_Palabras_mencionadas[i]
+                    
+                    Lista_de_la_palabra[b] = NuevoArreglo_Palabra[b]
+
+                    print()
+                #Verificará si ganó el juego
+                if "_" in Lista_de_la_palabra:
+                    continue
+                else:
+                    system("cls")
+                    print("¡ Felicidades ha gado el juego ! ")
+                    break
+
+            else:
+                print("la palabra no existe ")
+                cantidad_intentos = cantidad_intentos + 1
+        ####################################
 
     elif Opcion == "3":
         system("cls")
         print("has elegido la categoria Musica")
 
+         ###########################################################
+        cantidad_intentos = 0
+        system("cls")
+        
+
+        #logica del juego! :D
+        Palabra_Aleatoria = random.choice(M[2])
+        Lista = len( Palabra_Aleatoria)
+        indice = M[2].index(Palabra_Aleatoria)
+
+        Lista_de_la_palabra = []
+        #imprime las barras de abajo
+        for i in range(Lista):
+            Lista_de_la_palabra.append("_")
+
+        while True:
+            system("cls")
+            print("has elegido la Musica")
+            #imprime el monigote
+            funcionDelmuñeco()
+            if cantidad_intentos == 10:
+                print("el juego ha terminado")
+                break
+
+
+            #imprime el arrglo
+            for i in range(Lista):
+                print(Lista_de_la_palabra[i],end = ' ')
+            print("")
+            
+
+            Letra = input("Ingresa una letra > ")
+
+            #Este convierte la palabra optenida a una lista
+            
+            NuevoArreglo_Palabra = list(Palabra_Aleatoria)
+            
+            #condiciona si la letra exite dentro del arreglo
+            if Letra in M[2][indice]:
+                print("Palabra corecta")
+
+                #esta buscará la palabra y la colocará en la posicion
+                def Indices(lst,item):
+                    return [i for i,x in enumerate(lst) if x ==item]
+                Indices_Palabras_mencionadas = Indices(NuevoArreglo_Palabra,Letra)
+
+                Cuenta_Areglo = len(Indices_Palabras_mencionadas)
+
+                #coloca las palabras en el arreglo
+                for i in range(Cuenta_Areglo):
+
+                    b = Indices_Palabras_mencionadas[i]
+                    
+                    Lista_de_la_palabra[b] = NuevoArreglo_Palabra[b]
+
+                    print()
+                #Verificará si ganó el juego
+                if "_" in Lista_de_la_palabra:
+                    continue
+                else:
+                    system("cls")
+                    print("¡ Felicidades ha gado el juego ! ")
+                    break
+
+            else:
+                print("la palabra no existe ")
+                cantidad_intentos = cantidad_intentos + 1
+        ####################################
+
     elif Opcion == "4":
         system("cls")
         print("has elegido la categoria Animales")
 
+        ###########################################################
+        cantidad_intentos = 0
+        system("cls")
+        
+
+        #logica del juego! :D
+        Palabra_Aleatoria = random.choice(M[3])
+        Lista = len( Palabra_Aleatoria)
+        indice = M[3].index(Palabra_Aleatoria)
+
+        Lista_de_la_palabra = []
+        #imprime las barras de abajo
+        for i in range(Lista):
+            Lista_de_la_palabra.append("_")
+
+        while True:
+            system("cls")
+            print("Animales")
+            #imprime el monigote
+            funcionDelmuñeco()
+            if cantidad_intentos == 10:
+                print("el juego ha terminado")
+                break
+
+
+            #imprime el arrglo
+            for i in range(Lista):
+                print(Lista_de_la_palabra[i],end = ' ')
+            print("")
+            
+
+            Letra = input("Ingresa una letra > ")
+
+            #Este convierte la palabra optenida a una lista
+            
+            NuevoArreglo_Palabra = list(Palabra_Aleatoria)
+            
+            #condiciona si la letra exite dentro del arreglo
+            if Letra in M[3][indice]:
+                print("Palabra corecta")
+
+                #esta buscará la palabra y la colocará en la posicion
+                def Indices(lst,item):
+                    return [i for i,x in enumerate(lst) if x ==item]
+                Indices_Palabras_mencionadas = Indices(NuevoArreglo_Palabra,Letra)
+
+                Cuenta_Areglo = len(Indices_Palabras_mencionadas)
+
+                #coloca las palabras en el arreglo
+                for i in range(Cuenta_Areglo):
+
+                    b = Indices_Palabras_mencionadas[i]
+                    
+                    Lista_de_la_palabra[b] = NuevoArreglo_Palabra[b]
+
+                    print()
+                #Verificará si ganó el juego
+                if "_" in Lista_de_la_palabra:
+                    continue
+                else:
+                    system("cls")
+                    print("¡ Felicidades ha gado el juego ! ")
+                    break
+
+            else:
+                print("la palabra no existe ")
+                cantidad_intentos = cantidad_intentos + 1
+        ####################################
+
     elif Opcion == "5":
         system("cls")
         print("has elegido la operacion Verduras")
+
+         ###########################################################
+        cantidad_intentos = 0
+        system("cls")
+        
+
+        #logica del juego! :D
+        Palabra_Aleatoria = random.choice(M[4])
+        Lista = len( Palabra_Aleatoria)
+        indice = M[4].index(Palabra_Aleatoria)
+
+        Lista_de_la_palabra = []
+        #imprime las barras de abajo
+        for i in range(Lista):
+            Lista_de_la_palabra.append("_")
+
+        while True:
+            system("cls")
+            print("has elegido la categoria Frutas")
+            #imprime el monigote
+            funcionDelmuñeco()
+            if cantidad_intentos == 10:
+                print("el juego ha terminado")
+                break
+
+
+            #imprime el arrglo
+            for i in range(Lista):
+                print(Lista_de_la_palabra[i],end = ' ')
+            print("")
+            
+
+            Letra = input("Ingresa una letra > ")
+
+            #Este convierte la palabra optenida a una lista
+            
+            NuevoArreglo_Palabra = list(Palabra_Aleatoria)
+            
+            #condiciona si la letra exite dentro del arreglo
+            if Letra in M[4][indice]:
+                print("Palabra corecta")
+
+                #esta buscará la palabra y la colocará en la posicion
+                def Indices(lst,item):
+                    return [i for i,x in enumerate(lst) if x ==item]
+                Indices_Palabras_mencionadas = Indices(NuevoArreglo_Palabra,Letra)
+
+                Cuenta_Areglo = len(Indices_Palabras_mencionadas)
+
+                #coloca las palabras en el arreglo
+                for i in range(Cuenta_Areglo):
+
+                    b = Indices_Palabras_mencionadas[i]
+                    
+                    Lista_de_la_palabra[b] = NuevoArreglo_Palabra[b]
+
+                    print()
+                #Verificará si ganó el juego
+                if "_" in Lista_de_la_palabra:
+                    continue
+                else:
+                    system("cls")
+                    print("¡ Felicidades ha gado el juego ! ")
+                    break
+
+            else:
+                print("la palabra no existe ")
+                cantidad_intentos = cantidad_intentos + 1
+        ####################################
     else:
         print("No seas Bobo la opcion no existe!")
 
--- Modulo1/6_Programitas/a.py
+++ Modulo1/6_Programitas/a.py
@@ -1,7 +1,7 @@
-from itertools import count
+from itertools import count         
 
 
-list = ['a','a','l','e','b']
+list1 = ['i','a','o','e','a']
 
 list2 = ["0","0","0","0","0"]
 
@@ -9,14 +9,20 @@ list2 = ["0","0","0","0","0"]
 def indices(lst, item):
     return [i for i, x in enumerate(lst) if x == item]
 
-a = indices(list, "a")
+a = indices(list1, "a")
 print(a)
 
-
-for i in a:
-    list2[i] = a
-
+cuenta = len(a)
+for i in range(cuenta):
+    b = a[i]
+    list2[b]= list1[b]
 
 print("")
 for i in list2:
     print(i)
+
+
+# hola = "hola"
+# hola2 = list("hola")
+
+# print(hola2)
