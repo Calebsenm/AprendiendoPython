4739bc650d05ec6e14227acbeae939c05424e4d3
caleb <calebgamercraft@gmail.com>
2022-04-30 22:13:43 -0500

    movimiento y optimizacion

--- Conceptos/9_POO/9_POO_Ejercicios/1_1Ejercicio.py
+++ Conceptos/9_POO/9_POO_Ejercicios/1_1Ejercicio.py
@@ -26,145 +26,140 @@ M=[
     [" ","_","_","_","_","_","_","_","_","_","_"," "],
     [" "," ","A","B","C","D","E","F","G","H"," "," "]
 ]
-Contador_cambio = 1
+M.reverse()
+for a in range(12):
+    M[a].reverse()
+    for b in range(12):
+        a = 0
 
-while True:
-  
-    if Contador_cambio == 1:
-        system("cls")
+#funcion de la operacion
+   
+def Accion(jugador,n1,n2,n3,n4,n5,n6,n7,n8,n11,n12,n13,n14,n15,n16,n17,n18):
+    system("cls")
         
-        print("Las fichas..")
-        print(". ".join(B))
-        print(". ".join(N))
-        print()
+    print("Las fichas..")
+    print(". ".join(B))
+    print(". ".join(N))
+    print()
 
 
-        for i in range(12):
-            for j in range(12):
-                print(M[i][j],end = ' ') 
-            print()
-
-        #pide la posicion de la ficha
-        Numero_fila = int(input("Ingrese EL numero de la ficha >> "))
-        Letra_columna = input("Ingrese La letra de la ficha >> ")
+    M.reverse()
         
-        if Numero_fila == 1:
-            fila = 9
-        
-        #coloca las filas
-        elif Numero_fila == 2:
-            fila = 8
-        elif Numero_fila == 3:
-            fila = 7
-        elif Numero_fila == 4:
-            fila =6
-        elif Numero_fila == 5:
-            fila =5
-        elif Numero_fila == 6:
-            fila =4
-        elif Numero_fila == 7:
-            fila =3
-        elif Numero_fila == 8:
-            fila =2
-        else:
-            print("error")
-        #ubica las colunas
-        if Letra_columna == "A":
-            columna = 2
-        elif Letra_columna == "B":
-            columna = 3
-        elif Letra_columna == "C":
-            columna = 4
-        elif Letra_columna == "D":
-            columna = 5
-        elif Letra_columna == "E":
-            columna = 6
-        elif Letra_columna == "F":
-            columna = 7
-        elif Letra_columna == "G":
-            columna = 8
-        elif Letra_columna == "H":
-            columna = 9
-        
-
-        Ficha = M[fila][columna]
-        print(Ficha)
-
-        Letra = input("Letra donde Desea mover la ficha>> ")
-        # Numero = input("Numero donde Desea Mover la ficha >> ")
-
-        #hace el cambio
-        Contador_cambio = 0
-
-    elif Contador_cambio == 0:
-        system("cls")
-        Contador_cambio = 1
-        print("Las fichas..")
-        print(". ".join(B))
-        print(". ".join(N))
-        print()
-
-
-        M.reverse()
-        
-        for i in range(12):
+    for i in range(12):
             
-            M[i].reverse()
-            for j in range(12):
-                print(M[i][j],end = ' ') 
-            print()
+        M[i].reverse()
+        for j in range(12):
+            print(M[i][j],end = ' ') 
+        print()
 
-        Numero_fila2 = int(input("Ingrese EL numero de la ficha >> "))
-        Letra_columna2 = input("Ingrese La letra de la ficha >> ")
+    print(jugador)
+    Numero_fila2 = int(input("Ingrese EL numero de la ficha >> "))
+    Letra_columna2 = input("Ingrese La letra de la ficha >> ")
 
-        if Numero_fila2 == 8:
-            fila2 = 2
-        elif Numero_fila2 ==7:
-            fila2 = 3
-        elif Numero_fila2 ==6:
-            fila2 = 4
-        elif Numero_fila2 ==5:
-            fila2 = 5
-        elif Numero_fila2 ==4:
-            fila2 = 6
-        elif Numero_fila2 ==3:
-            fila2 =7
-        elif Numero_fila2 == 2:
-            fila2 = 8
-        elif Numero_fila2 ==1:
-            fila2 = 9
-        else:
-            print("Error")
+    
+    if Numero_fila2 == 8:
+        fila2 = n1
+    elif Numero_fila2 ==7:
+        fila2 = n2
+    elif Numero_fila2 ==6:
+        fila2 = n3
+    elif Numero_fila2 ==5:
+        fila2 = n4
+    elif Numero_fila2 ==4:
+        fila2 = n5
+    elif Numero_fila2 ==3:
+        fila2 = n6
+    elif Numero_fila2 == 2:
+        fila2 = n7
+    elif Numero_fila2 ==1:
+        fila2 = n8
+    else:
+        print("Error")
         
-        #columnas
-        if Letra_columna2 == "A":
-            columna2 = 9
-        elif Letra_columna2 =="B":
-            columna2 = 8
-        elif Letra_columna2 =="C":
-            columna2 = 7
-        elif Letra_columna2 == "D":
-            columna2 = 6
-        elif Letra_columna2 == "E":
-            columna2 = 5
-        elif Letra_columna2 == "F":
-            columna2 = 4
-        elif Letra_columna2 == "G":
-            columna2 = 3
-        elif Letra_columna2 == "H":
-            columna2 = 2
-        else: 
-            print("Eroor")
-        
-
-        Ficha2 = M[fila2][columna2]
-        print(Ficha2)
-
-        Letra = input("Letra donde Desea mover la ficha>> ")
-        # Numero = input("Numero donde Desea Mover la ficha >> ")
-
-        #hace el cambio
+    #columnas
+    if Letra_columna2 == "A":
+        columna2 = n11
+    elif Letra_columna2 =="B":
+        columna2 = n12
+    elif Letra_columna2 =="C":
+        columna2 = n13
+    elif Letra_columna2 == "D":
+        columna2 = n14
+    elif Letra_columna2 == "E":
+        columna2 = n15
+    elif Letra_columna2 == "F":
+        columna2 = n16
+    elif Letra_columna2 == "G":
+        columna2 = n17
+    elif Letra_columna2 == "H":
+        columna2 = n18
+    else: 
+        print("Eroor")
         
 
+    Ficha2 = M[fila2][columna2]
+    print(Ficha2)
+
+    
+            
+    Numero_fila2 = int(input("Numero donde Desea Mover la ficha >> "))
+    Letra_columna2 = input("Letra donde Desea mover la ficha>> ")
+    #hace el cambio
+    if Numero_fila2 == 8:
+        fila2 = n1
+    elif Numero_fila2 ==7:
+        fila2 = n2
+    elif Numero_fila2 ==6:
+        fila2 = n3
+    elif Numero_fila2 ==5:
+        fila2 = n4
+    elif Numero_fila2 ==4:
+        fila2 = n5
+    elif Numero_fila2 ==3:
+        fila2 = n6
+    elif Numero_fila2 == 2:
+        fila2 = n7
+    elif Numero_fila2 ==1:
+        fila2 = n8
+    else:
+        print("Error")
+        
+    #columnas
+    if Letra_columna2 == "A":
+        columna2 = n11
+    elif Letra_columna2 =="B":
+        columna2 = n12
+    elif Letra_columna2 =="C":
+        columna2 = n13
+    elif Letra_columna2 == "D":
+        columna2 = n14
+    elif Letra_columna2 == "E":
+        columna2 = n15
+    elif Letra_columna2 == "F":
+        columna2 = n16
+    elif Letra_columna2 == "G":
+        columna2 = n17
+    elif Letra_columna2 == "H":
+        columna2 = n18
+    else: 
+        print("Eroor")
+    #el peon y su movimiento
+    
+    M[fila2][columna2] = Ficha2 
+   
+
+
+
+
+Contador_cambio = 2
+while True:
+    
+    if Contador_cambio %2 == 0:
+
+        Accion("Jugador1",2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9)
+
+    else:
+        Accion("Jugador2",9,8,7,6,5,4,3,2,9,8,7,6,5,4,3,2)
+    Contador_cambio = Contador_cambio + 1
 
 
